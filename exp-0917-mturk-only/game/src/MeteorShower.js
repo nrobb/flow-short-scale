6375f5cfd9cb9dec1ce87f1d15f2af7850872e57
Nigel Robb <nrobb@users.noreply.github.com>
2017-09-22 19:48:16 +0100

    Update MeteorShower.js

--- exp-0917-mturk-only/game/src/MeteorShower.js
+++ exp-0917-mturk-only/game/src/MeteorShower.js
@@ -107,6 +107,7 @@ DDATest.MeteorShower.prototype.create = function() {
   localStorage.setItem("data", "");
   this.gameOver = false;
   this.setupExperiemt();
+  this.endGame(); //hack for testing only
 };
 
 DDATest.MeteorShower.prototype.update = function() {
