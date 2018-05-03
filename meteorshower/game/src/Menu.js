949a28bfb6e841b0fd9d3456ee205d61b4cd4538
Nigel Robb <nrobb@users.noreply.github.com>
2018-05-03 13:39:23 +0900

    Update Menu.js

--- meteorshower/game/src/Menu.js
+++ meteorshower/game/src/Menu.js
@@ -17,11 +17,11 @@ DDATest.Menu.prototype.create = function() {
     this.state.start('MeteorShower');
   }, this);
   // instructions
-  this.add.bitmapText(250, 20, 'carrier-command', 'METEOR SHOWER', 15);
-  this.add.bitmapText(250, 50, 'carrier-command', 'USE THE LEFT AND RIGHT ARROWS TO MOVE', 10);
-  this.add.bitmapText(250, 80, 'carrier-command', 'GET 1 POINT FOR EVERY STAR YOU CAN CATCH', 10);
-  this.add.bitmapText(250, 110, 'carrier-command', 'AVOID THE METEORS!!', 10);
-  this.add.bitmapText(250, 140, 'carrier-command', 'HITTING A METEOR MEANS YOU LOSE A POINT', 10);
+  this.add.bitmapText(300, 20, 'carrier-command', 'METEOR SHOWER', 20);
+  this.add.bitmapText(250, 80, 'carrier-command', 'USE THE LEFT AND RIGHT ARROWS TO MOVE', 10);
+  this.add.bitmapText(250, 110, 'carrier-command', 'GET 1 POINT FOR EVERY STAR YOU CAN CATCH', 10);
+  this.add.bitmapText(250, 140, 'carrier-command', 'AVOID THE METEORS!!', 10);
+  this.add.bitmapText(250, 170, 'carrier-command', 'HITTING A METEOR MEANS YOU LOSE A POINT', 10);
   this. startText = this.add.bitmapText(this.world.centerX, this.world.centerY, 'carrier-command', 'CLICK HERE TO START', 20);
   this.startText.anchor.set(0.5, 0.5);
   // copyright
